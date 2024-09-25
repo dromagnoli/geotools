c8518ec5f05657b1b13cb238f9081aeff79e0845
Daniele Romagnoli <daniele.romagnoli@geosolutionsgroup.com>
2024-09-25 16:02:29 +0200

    More on PROJ

---
+++ modules/library/referencing/src/main/java/org/geotools/referencing/util/PROJFormattable.java
@@ -0,0 +1,8 @@
+package org.geotools.referencing.util;
+
+import org.geotools.referencing.proj.PROJFormatter;
+
+public interface PROJFormattable {
+
+    String formatPROJ(PROJFormatter formatter);
+}
