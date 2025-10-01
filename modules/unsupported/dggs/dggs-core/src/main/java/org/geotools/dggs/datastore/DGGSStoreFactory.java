9ebb9e0450cc52eca71d8d648552221c198dbfaf
Daniele Romagnoli <daniele.romagnoli@geosolutionsgroup.com>
2025-10-01 17:52:43 +0200

    Setting up DGGS modules

---
+++ modules/unsupported/dggs/dggs-core/src/main/java/org/geotools/dggs/datastore/DGGSStoreFactory.java
@@ -0,0 +1,122 @@
+/*
+ *    GeoTools - The Open Source Java GIS Toolkit
+ *    http://geotools.org
+ *
+ *    (C) 2020, Open Source Geospatial Foundation (OSGeo)
+ *
+ *    This library is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU Lesser General Public
+ *    License as published by the Free Software Foundation;
+ *    version 2.1 of the License.
+ *
+ *    This library is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Lesser General Public License for more details.
+ */
+package org.geotools.dggs.datastore;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.geotools.api.data.DataStore;
+import org.geotools.api.data.DataStoreFactorySpi;
+import org.geotools.api.data.Repository;
+import org.geotools.api.feature.type.Name;
+import org.geotools.dggs.DGGSFactoryFinder;
+import org.geotools.dggs.DGGSInstance;
+import org.geotools.feature.NameImpl;
+import org.geotools.jdbc.JDBCDataStoreFactory;
+
+/**
+ * Factory for {@link org.geotools.dggs.gstore.DGGSStore} based on ClickHouse storage . TODO: generalize this so that it
+ * can take DGGS parameters as well.
+ */
+// TODO: add a limit to the complexity of queries the store is willing to accept? And suggest the
+// user to switch to a lower resolution instead. Though maybe this ought to be done at the
+// service level, and propagated down to the store as a Hint?
+public class DGGSStoreFactory implements DataStoreFactorySpi {
+
+    /** parameter for database type */
+    // TODO: find some better way to separate this from the DGGSGeometryStore?
+    public static final Param DGGS_FACTORY_ID =
+            new Param("dggs_id", String.class, "DGGS Factory identifier, e.g., H3 or rHEALPix", true, null);
+
+    /** Logical store name (used by your store to resolve tables/collections/etc.) */
+    public static final Param STORE_NAME =
+            new Param("store_name", String.class,
+                    "Logical store name for this repository (schema, namespace, etc.)",
+                    true, null);
+
+    /** Repository instance to be used by the store.*/
+    public static final Param REPOSITORY =
+            new Param("repository", Repository.class,
+                    "Repository implementation used by the store (passed by reference)",
+                    true, null);
+
+    public static final Param ZONE_COLUMN_IDENTIFIER =
+            new Param("zoneColumnIdentifier", String.class,
+                    "The column name identifying the column containing the DGGS zone identifiers",
+                    true, null);
+
+    @Override
+    public DataStore createNewDataStore(Map<String, ?> params) throws IOException {
+        return createDataStore(params);
+    }
+
+    @Override
+    public DataStore createDataStore(Map<String, ?> params) throws IOException {
+        // setup the DGGS instance
+        String factoryId = (String) DGGS_FACTORY_ID.lookUp(params);
+        DGGSInstance dggs = DGGSFactoryFinder.createInstance(factoryId, params);
+
+        String storeName = (String) STORE_NAME.lookUp(params);
+        Repository repository = (Repository) REPOSITORY.lookUp(params);
+        if (repository == null) {
+            throw new IOException("Parameter 'repository' is required and must be non-null.");
+        }
+        if (storeName == null || storeName.isEmpty()) {
+            throw new IOException("Parameter 'store_name' is required and must be non-empty.");
+        }
+
+        String zoneColumnId = (String) ZONE_COLUMN_IDENTIFIER.lookUp(params);
+
+        /*Map<String, Object> delegateParams = new HashMap<>(params);
+        delegateParams.put(JDBCDataStoreFactory.DBTYPE.key, DATABASE_ID);
+        delegateParams.put(JDBCDataStoreFactory.SCHEMA.key, params.get(JDBCDataStoreFactory.DATABASE.key));*/
+        Name name = new NameImpl(storeName);
+        DataStore datastore = repository.dataStore(name);
+
+
+        return new DGGSDataStore(dggs, datastore, zoneColumnId);
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Generic Repository + DGGS integration";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Builds a DGGS-backed store using a provided Repository and store name.";
+    }
+
+    @Override
+    public Param[] getParametersInfo() {
+        /*Stream<Param> delegateParams = Stream.of(delegate.getParametersInfo())
+                .filter(p -> !JDBCDataStoreFactory.DBTYPE.key.equals(p.key)
+                        && !JDBCDataStoreFactory.SCHEMA.key.equals(p.key));
+        return Stream.concat(Stream.of(DGGS_FACTORY_ID), delegateParams).toArray(n -> new Param[n]);*/
+        return new Param[] {DGGS_FACTORY_ID, STORE_NAME, REPOSITORY, ZONE_COLUMN_IDENTIFIER};
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return DGGSFactoryFinder.getExtensionFactories().findAny().isPresent();
+    }
+
+    /*
+    protected DataSource createDataSource(Map<String, ?> params) throws IOException {
+        return delegate.createDataSource(params);
+    }*/
+}
